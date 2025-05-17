8e53133ea2688b90d5e905e6f36d5870f546e390
Mikel <mikel.kunze887@web.de>
2025-05-17 22:29:40 +0200

    Basic crawling works

--- Program.cs
+++ Program.cs
@@ -1,12 +1,71 @@
-﻿
-namespace Program;
+﻿using System.Diagnostics;
 
+namespace legoog.Services;
 
-public class PMain {
+public class Crwaler {
 
-    public static void Main(string[] args) {
+    const string pathconst = @"C:\tmp\crawlerdata\";
+    const string linkParam = "href=";
 
-
+    // just for testing --> gets replaced as soon as there is a web project :)
+    public static void Main() {
+        Crwaler cr = new();
+        cr.getAllFiles();
+        
     }
 
+    private void getAllFiles() {
+        var files = Directory.GetFiles(pathconst);
+        foreach(var file in files) {
+            Thread thread = new Thread(() => getAllLinks(file));
+            thread.Start();
+        }
+    }
+
+    private void getAllLinks(string filename) {
+        try {
+            using StreamReader sr = new StreamReader(filename);
+            string content = sr.ReadToEnd();
+            var lines = content.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
+            foreach (var line in lines) {
+                if (line.Contains(linkParam)) {
+                    Thread thread = new Thread(() => getUrl(line));
+                    thread.Start();
+                }
+            }
+        } catch(Exception e) {
+            throw new Exception(e.Message);
+        }
+    }
+
+    private void getUrl(string itemURl) {
+        string firstSplit = "href=\"";
+        string secontSplit = "\"";
+
+        string[] first = itemURl.Split(firstSplit);
+        string[] url = first[1].Split(secontSplit);
+        if(url[0].Contains("https://")) DownHtml(url[0]).Wait();
+    }
+
+    // start with new Thread --> better performance
+    private async Task DownHtml(string url) {
+        try {
+            string file = fileName(url);
+            using HttpClient httpClient = new();
+            var response = await httpClient.GetAsync(new Uri(url));
+            response.EnsureSuccessStatusCode();
+            string htmlText = await response.Content.ReadAsStringAsync();
+            if(File.Exists(Path.Combine(pathconst, file))) File.Delete(Path.Combine(pathconst, file));
+            File.WriteAllText(Path.Combine(pathconst, file), htmlText);
+        } catch(Exception e) {
+            Debug.WriteLine(e.Message);
+        }
+    }
+
+    private string fileName(string url) {
+        string split = "https://";
+        string[] splitetURL = url.Split(split); 
+        string fileName = splitetURL[1].Replace(".", "").Replace("/", "").Replace("\\", "").Replace(":", "").Replace("?", "").Replace("&", "").Replace("=", "")+ ".txt";
+        return fileName;
+    }
 }
