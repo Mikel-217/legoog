37b85694fb752cc1b5861b27e9b790fc7ce5875b
Mikel-217 <mikel.kunze887@web.de>
2025-05-21 10:36:47 +0200

    Added Readme | DB-Service changes

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Desing
+1. It crawles from website to website --> searches for links at the downloaded website
+2. Now it gets the title and a keyword --> keyword algorithm musst be implenentet first
+3. Safes the url in an Database with the keyword and keyword count (higher the keyword higher the ranking)
+
+
+# Keyword algorithm - idea
+searches for words which are simular to the title
+
+# Other ideas:
+- stores the website Text also in a Database and searches throw the text to look for a simular result
--- Services/DBService.cs
+++ Services/DBService.cs
@@ -1,17 +1,45 @@
 using System.Data.Common;
 using legoog.Services;
 using legoog.Models;
+using Microsoft.Data.SqlClient;
 
 namespace legoog.Services.DB;
 
-public class DBService
+public class DBService 
 {
     private Data? data;
+
+    // later safe it in a ressource file
+    string connectionString = @"";
+
+    private SqlConnection sqlConn()
+    {
+        try
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            return conn;
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+
+    }
+
 
     public void insertNewSearchResult()
     {
         // write DB Connection and insert all data    
-        
+        var dbCon = sqlConn();
+        dbCon.CreateCommand();
+        dbCon.Close();
     }
+
+
+    // public Data getSearch()
+    // {
+    //     return null; 
+    // }
 
 }
